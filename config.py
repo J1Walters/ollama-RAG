10f177dca9164b2c6924167aaca75b90e32399cf
J1Walters <waltersjuk@gmail.com>
2025-01-05 15:41:31 +0000

    Add chunk size

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 PDF_DIR_PATH = './files_for_retrieval'
-EMBEDDING_MODEL = HuggingFaceEmbedding(model_name='dunzhang/stella_en_400M_v5')
+EMBEDDING_MODEL = HuggingFaceEmbedding(model_name='dunzhang/stella_en_400M_v5', trust_remote_code=True)
+CHUNK_SIZE = 1024
