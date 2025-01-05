1e95d8ebd5f3f6a4678d37df358bfef22f5b09bc
J1Walters <waltersjuk@gmail.com>
2025-01-05 15:32:28 +0000

    Change embedding model

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 PDF_DIR_PATH = './files_for_retrieval'
-EMBEDDING_MODEL = HuggingFaceEmbedding(model_name='dunzhang/stella_en_1.5B_v5')
+EMBEDDING_MODEL = HuggingFaceEmbedding(model_name='dunzhang/stella_en_400M_v5')
